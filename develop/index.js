e891dcfc7d302bd54602000005d6cc1c53a225e5
B Water <brennw47@gmail.com>
2024-05-02 15:53:40 -0700

    Update index.js so that there is more questions, added a list for fonts in the array, a number entry prompt for size of text, a list for selection of the shapes color, a list for selected yes or no on if the user wants a border or not may have to change to confirm not sure yet because jest did not install might be my .gitignore file, a list for colors for the border, a number entry for the border size, and a list selection for what style of border, added an init function to handle the prompted questions and then handle the writing of the answers by making a variable for data and passing the generateSvgLogo function into the init function then having the file with the data however i'm not sure if the it runs yet because I cant get the packages to install however I will fix, and added comments/Updated shapes.js to include exports for the classes that i will build out, and an export for the generateSvgLogo function, added an if statement to check if the data for the shape selected in the prompt is one of the shapes if so make a new shape, added an if statement for the border selection so that if the user wants a border then the shape will get a border if yes is selected then once yes is selected the if statement here created a new border style for the shape, and then sets the color size and style for the user, then the return function generates the logo.svg file with the shape however that shape that is returned does not contain all of the spots for the styling yet so it will not generate properly.

--- develop/index.js
+++ develop/index.js
@@ -1,5 +1,7 @@
 // Runs the application using imports from lib
-// import { lib } from './lib';
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateSvgLogo = require("./lib/shapes");
 // This is the array of questions that will be asked to the user
 const questions = [
   {
@@ -9,8 +11,19 @@ const questions = [
   },
   {
     type: "list",
-    name: "Color",
-    message: "What color do you want the logo to be?",
+    name: "Font",
+    message: "What font do you want the text to be?",
+    choices: ["Arial", "Times New Roman", "Courier New"],
+  },
+  {
+    type: "number",
+    name: "Size",
+    message: "What size do you want the text to be?",
+  },
+  {
+    type: "list",
+    name: "TextColor",
+    message: "What color do you want the text to be?",
     choices: ["blue", "red", "green"],
   },
   {
@@ -20,12 +33,41 @@ const questions = [
     choices: ["Triangle", "Circle", "Square"],
   },
   {
+    type: "list",
+    name: "ShapeColor",
+    message: "What color do you want the logo to be?",
+    choices: ["blue", "red", "green"],
+  },
+  {
+    type: "list",
+    name: "Border",
+    message: "Do you want a border around the logo?",
+    choices: ["Yes", "No"],
+  },
+  {
+    type: "list",
+    name: "BorderColor",
+    message: "What color do you want the border to be?",
+    choices: ["blue", "red", "green"],
+  },
+  {
+    type: "number",
+    name: "BorderSize",
+    message: "What size do you want the border to be?",
+  },
+  {
+    type: "list",
+    name: "BorderStyle",
+    message: "What style do you want the border to be?",
+    choices: ["solid", "dotted", "dashed"],
+  },
+  {
     type: "confirm",
     name: "Save",
     message: "Do you want to save the logo?",
   },
 ];
-
+// This is the function that will be called when the user answers the questions and then generates the logo with the answers
 function writeToFile(data) {
   fs.writeFile("logo.svg", data, (err) => {
     if (err) {
@@ -37,3 +79,14 @@ function writeToFile(data) {
     }
   });
 };
+// This is the function that initializes the application and asks the user the questions in the questions array and
+// then generates the logo with the generateSvgLogo function which in turn passes along the data from the questions for the write to file function to be saved.
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    const data = generateSvgLogo(answers);
+    writeToFile(data);
+  });
+};
+
+init();
--- develop/lib/shapes.js
+++ develop/lib/shapes.js
@@ -32,9 +32,29 @@ class Triangle {
 //         return `<rect x="50" y="50" width="200" height="200" fill="${this.color}" />`;
 //     }
 // }
+
 // Function to generate SVG logo using data from the shape classes in shapes.js
 generateSvgLogo(data) {
-    const shape = new Triangle();
+    let shape;
+    if (data.Shape === "Triangle") {
+        shape = new Triangle();
+    } else if (data.Shape === "Circle") {
+        shape = new Circle();
+    } else if (data.Shape === "Square") {
+        shape = new Square();
+    }
+    if (data.Shape.Border === "Yes") {
+        shape = new Border();
+        shape.setBorderColor(data.BorderColor);
+        shape.setBorderSize(data.BorderSize);
+        shape.setBorderStyle(data.BorderStyle);
+    }
+    shape.setColor(data.Color);
     return `<svg width="300" height="300">${shape.render()}</svg>
     `
 }
+
+module.exports = Circle;
+module.exports = Square;
+module.exports = generateSvgLogo(data);
+module.exports = Triangle;
