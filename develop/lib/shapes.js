a8a2b8204f645a3bad6b330c97ed95771d82e116
B Water <brennw47@gmail.com>
2024-05-02 13:50:11 -0700

    Update shapes.js to comment out untested shapes so I can test the triangle and then have the triangle test fail, then I will pass the test and refactor.
    Update shapes.js to comment out untested shapes so I can test the triangle and then have the triangle test fail, then I will pass the test and refactor so I can know that my triangle class will work when my prompted questions ask about which color the user wants for the shape.

--- develop/lib/shapes.js
+++ develop/lib/shapes.js
@@ -10,25 +10,25 @@ class Triangle {
         return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
     }
 }
-class Circle {
-    constructor() {
-        this.color = "black";
-    }
-    setColor(color) {
-        this.color = color;
-    }
-    render() {
-        return `<circle cx="150" cy="150" r="100" fill="${this.color}" />`;
-    }
-}
-class Square {
-    constructor() {
-        this.color = "black";
-    }
-    setColor(color) {
-        this.color = color;
-    }
-    render() {
-        return `<rect x="50" y="50" width="200" height="200" fill="${this.color}" />`;
-    }
-}
+// class Circle {
+//     constructor() {
+//         this.color = "black";
+//     }
+//     setColor(color) {
+//         this.color = color;
+//     }
+//     render() {
+//         return `<circle cx="150" cy="150" r="100" fill="${this.color}" />`;
+//     }
+// }
+// class Square {
+//     constructor() {
+//         this.color = "black";
+//     }
+//     setColor(color) {
+//         this.color = color;
+//     }
+//     render() {
+//         return `<rect x="50" y="50" width="200" height="200" fill="${this.color}" />`;
+//     }
+// }
